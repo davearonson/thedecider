ed9cb61e702ca4247840ef8110f072b6a4cc9424
Dave Aronson <github2dave@davearonson.com>
2011-11-08 12:33:06 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thedecider::Application.config.secret_token = 'c3d19e980496e6c2f63a6e0977f3dafa9964546a30ef33b1888985bf13adc81ac59602e98f715fb54c659f2cd126ec3a2d93b4739f2c6b7c685adbdbfafdb3b6'
